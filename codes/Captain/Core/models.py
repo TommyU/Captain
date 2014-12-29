a056a15d35ff162fed3216ef467b05c7f91ccf20
tommy <tommy@ubuntu.(none)>
2014-12-29 23:02:34 +0800

    add base model class

--- codes/Captain/Core/models.py
+++ codes/Captain/Core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models impor User
 
 # Create your models here.
+class ModelBase(models.Model):
+    created_date = models.DateTimeField(auto_add=True)
+    creator = models.ForeignKey(User)
+    updated_date = models.DateTimeField(auto_now=True)
+    updator = models.ForeignKey(User)
+
+    class Meta:
+        abstract=True
+
+    def save(self, uid, ids, force_insert=False, force_update=False, using=None,update_fields=None):
+	#TODO:do some validation here
+        super(ModelBase,self).save(self, force_insert, force_update, using,update_fields)
