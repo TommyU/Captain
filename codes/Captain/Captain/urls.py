c34f7003cb824d1b3997c68ddc3c37b173085d16
Tommy <lion_awake@163.com>
2015-01-05 22:18:01 +0800

    base url for core changed to site base url

--- codes/Captain/Captain/urls.py
+++ codes/Captain/Captain/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^core/',include('Core.urls')),
+    url(r'^',include('Core.urls')),
 )
 
 if settings.DEBUG:
