fae26565b913e92f36746dcaaaee84dd1fd5d7fd
TommyU <lion_awake@163.com>
2015-01-05 22:11:37 +0800

    bootstrap introduced in font end

--- codes/Captain/Captain/urls.py
+++ codes/Captain/Captain/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,10 +9,11 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^core/',include('Core.urls')),
 )
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += patterns('',
         url(r'^__debug__/', include(debug_toolbar.urls)),
-)
+    )
--- codes/Captain/Core/views.py
+++ codes/Captain/Core/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
 
 # Create your views here.
+
+def bs_demo(req):
+	return render_to_response('bootstrap.all.html',{})
